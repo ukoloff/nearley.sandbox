2eb1e070b738d442ea45435a6b97e056853ce075
Stas Ukolov <ukoloff@gmail.com>
2019-03-05 19:33:19 +0500

    Timestamp all rules

--- elapse.js
+++ elapse.js
@@ -2,10 +2,24 @@ const nearley = require('nearley')
 const grammar = require('./cf')
 
 const gram = nearley.Grammar.fromCompiled(grammar)
-gram.start = 'right'
 
-const parser = new nearley.Parser(gram)
+for (let start of rules(gram)) {
+  gram.start = start
 
-console.time('parse')
-parser.feed('x'.repeat(2000))
-console.timeEnd('parse')
+  const parser = new nearley.Parser(gram)
+
+  console.time(start)
+  parser.feed('x'.repeat(3000))
+  console.timeEnd(start)
+}
+
+function rules(grammar) {
+  let seen = {}, result = []
+  for (let z of grammar.rules) {
+    if (/^\w+$/.test(z.name) && !seen[z.name]) {
+      seen[z.name] = 1
+      result.push(z.name)
+    }
+  }
+  return result.sort()
+}
